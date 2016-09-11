47456ea33303b79e3e469e2a67822e5f92dbdbd5
Krishcdbry <mohana.krishna@misport.com>
2016-09-11 05:39:16 +0530

    node module published

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # npm-array-unique
 Receives array as an input and returns the unique array and also an object whith array items and their occurences
+
+## Installation
+
+```bash
+npm i npm-array-unique
+```
+
+## Usage
+```javascript
+var arrayUnique = require('npm-array-unique');
+```
+
+## unique(array)
+
+Receives the input array and returns the unique array
+```javascript
+
+	var arrayUnique = require('npm-array-unique')
+	var inputArray = [1,2,3,4,3,2,4,3,4,5,4,5,5];
+	
+	arrayUnique.unique(inputArray);
+	// [ 1, 2, 3, 4, 5]
+	
+	
+	var inputArray2 = ['one', 5, 'two', 'three', 5, 'one', 'three', 7];
+	
+	arrayUnique.unique(inputArray2);
+	// ['one', 5, 'two','three', 7];
+  
+```
+
+## uniqueCount(text)
+
+Receives the input array and returns the unique array
+```javascript
+
+  var arrayUnique = require('npm-array-unique')
+  var inputArray = [1,2,3,4,4,4,4,5,6,7,7,8,8,8];
+ 
+  arrayUnique.uniqueCount(inputArray);
+  // { '1': 1, '2': 1, '3': 1, '4': 4, '5': 1, '6': 1, '7': 2, '8': 3 }
+ 
+  
+```
+
+## itemCount(text)
+
+Receives the input array and returns the unique array
+```javascript
+
+  var arrayUnique = require('npm-array-unique')
+  var inputArray = [1,2,3,4,2,3,4,2,3,4,5,6,7,8,9,0,6,7,8,9];
+ 
+  arrayUnique.itemCount(inputArray, 7);
+  // 2
+  
+```
+
+## Demo
+Demo @[npm-array-unique](https://tonicdev.com/npm/npm-array-unique)
+| https://tonicdev.com/npm/npm-array-unique
+
+## Author
+Krishcdbry [krishcdbry@gmail.com]
+
+## Licence
+MIT @[krishcdbry](krishcdbry.com)
---
+++ index.js
@@ -0,0 +1,97 @@
+'use strict'
+
+/**
+ * Which receives array as a input and returns the unique array and also an object whith array items and there occurences
+ *
+ * @module npm-array-unique
+ * @typicalname arrayUnique
+ * @example
+ * var unique = require('npm-array-unique')
+ */
+
+exports.unique = unique;
+exports.uniqueCount = uniqueCount;
+exports.itemCount = itemCount;
+
+
+/**
+ * @name unique
+ * @typicalname unique
+ * @param {array} arrData  - Input array data
+ * @return {array}
+ *
+ * @usage
+ * var arrayUnique = require('npm-array-unique')
+ * var inputArray = [1,2,3,4,3,2,4,3,4,5,4,5,5];
+ *
+ * arrayUnique.unique(inputArray);
+ * // [ 1, 2, 3, 4, 5]
+ *
+ */
+function unique(inputArr) {
+	var uniqueArr = [];
+
+	inputArr.map(function(item) {
+		if (uniqueArr.indexOf(item) === -1) {
+			uniqueArr.push(item);
+		}
+	});
+
+	return uniqueArr;
+}
+
+/**
+ * @name getCount
+ * @typicalname getCount
+ * @param {array} arrData  - Input array data
+ * @param {int/string} inputItem
+ * @param {int} count
+ * @return {int} count
+ *
+ * @usage
+ * var arrayUnique = require('npm-array-unique')
+ * var inputArray = [1,2,3,4,2,3,4,2,3,4,5,6,7,8,9,0,6,7,8,9];
+ *
+ * arrayUnique.itemCount(inputArray, 7);
+ * // 2
+ *
+ */
+function itemCount(inputArr, needle, count) {
+	var cloneArr = inputArr.slice();
+	var count = (count) ? count : 0;
+	var idx = cloneArr.indexOf(needle);
+
+	if (idx == -1) {
+		return count;
+	}
+	else {
+		count += 1;
+		cloneArr.splice(idx, 1);
+		return getCount(cloneArr, needle, count);
+	}
+}
+
+/**
+ * @name unique
+ * @typicalname unique
+ * @param {array} arrData  - Input array data
+ * @return {array}
+ *
+ * @usage
+ * var arrayUnique = require('npm-array-unique')
+ * var inputArray = [1,2,3,4,4,4,4,5,6,7,7,8,8,8];
+ *
+ * arrayUnique.uniqueCount(inputArray);
+ * // { '1': 1, '2': 1, '3': 1, '4': 4, '5': 1, '6': 1, '7': 2, '8': 3 }
+ *
+ */
+function uniqueCount(inputArr) {
+	var cloneArr = inputArr.slice();
+	var uniqueOccurrences = {};
+
+	cloneArr.map(function(item) {
+		uniqueOccurrences[item] = getCount(cloneArr, item);
+	});
+
+	return uniqueOccurrences;
+}
